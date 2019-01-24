29a995f6b3757b05e45d96248a19567b7c2a06c3
Reesys <emailjoshuareyes@gmail.com>
2019-01-24 12:26:35 -0700

    Created a controller folder and runner class.

---
+++ SampleTwoCPP/Controller/Runner.cpp
@@ -0,0 +1,15 @@
+//
+//  Runner.cpp
+//  SampleTwoCPP
+//
+//  Created by Reyes, Joshua on 1/24/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include <iostream>
+
+int main()
+{
+    
+    return 0;
+}
